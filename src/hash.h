d8682ca6968153b7e118a50125bc1ea4f1a0ae7a
sriharsha_madala <sriharsha.madala@gmail.com>
2018-09-14 07:45:10 -0500

    Added googletest as a submodule.

---
+++ src/hash.h
@@ -0,0 +1,30 @@
+#ifndef HASH_H
+#define HASH_H
+
+#include <iostream>
+#include <string>
+#include <list>
+
+using namespace std;
+
+struct node {
+  string key;
+  string value;
+};
+
+class HashTable {
+  private:
+    list<struct node> *hash_map_;
+    int nf_buckets_;
+	public:
+    HashTable(int nf_buckets);
+    HashTable();
+    ~HashTable();
+    void printHashTable();
+    void insert (string key, string value);
+    int hashingFunction (string key);
+    struct node * findEntry(string key);
+    void removeEntry(string key);
+};
+
+#endif
