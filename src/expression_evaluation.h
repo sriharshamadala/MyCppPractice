bdee05231694adb7b7c0f833a1b1afcf4837b2af
sriharsha_madala <sriharsha.madala@gmail.com>
2018-09-23 16:05:46 -0500

    Added infix to postfix expression conversion code. Handles paranthesis as well.

--- src/expression_evaluation.h
+++ src/expression_evaluation.h
@@ -2,6 +2,7 @@
 #include <stack>
 #include <sstream>
 #include <string>
+#include <exception>
 
 using namespace std;
 
@@ -12,9 +13,38 @@ bool isOperator (string token) {
     if ((temp_char == '+') ||
         (temp_char == '-') ||
         (temp_char == '*') ||
-        (temp_char == '/')) {
+        (temp_char == '/') ||
+        (temp_char == '^')) {
       result = true;
     }
+  }
+  return result;
+}
+
+int getOperatorPriority (char oper) {
+  int result = 1;
+  switch (oper) {
+    case '(':
+      result = 4;
+      break;
+    case ')':
+      result = 4;
+      break;
+    case '^':
+      result = 3;
+      break;
+    case '*':
+      result = 2;
+      break;
+    case '/':
+      result = 2;
+      break;
+    case '+':
+      result = 1;
+      break;
+    case '-':
+      result = 1;
+      break;
   }
   return result;
 }
@@ -42,6 +72,9 @@ T executeOperation (T op1, T op2, char oper) {
       } catch (exception e) {
         cout << "Exception: " << e.what() << endl;
       }
+      break;
+    case '^':
+      // TODO Need to handle exponent operator.
       break;
   }
   return result;
@@ -94,3 +127,71 @@ bool isValidPostfixExpression (string expression, char delimiter = ' ') {
   return true;
 }
 
+bool isHigherPrecedence (char oper_token, char oper_stack) {
+  int priority_token = getOperatorPriority(oper_token);
+  int priority_stack = getOperatorPriority(oper_stack);
+  // the case of equality is resolved using left to right associativity.
+  // an equal priority operator should also resolve the expression thus far.
+  return (priority_token > priority_stack);
+}
+
+bool isOpeningParanthesis (string token) {
+  return ((token.length() == 1) && (token[0] == '('));
+}
+
+bool isOpeningParanthesis (char oper_token) {
+  return (oper_token == '(');
+}
+
+bool isClosingParanthesis (string token) {
+  return ((token.length() == 1) && (token[0] == ')'));
+}
+
+string infixToPostfix (string infix_expression, char delimiter = ' ') {
+  string postfix_expression;
+  istringstream iss(infix_expression);
+  string token;
+  stack<char> oper_stack;
+
+  while (getline(iss, token, delimiter)) {
+    if (isOperator(token)) {
+      char oper_token = token[0];
+      while ((!oper_stack.empty()) &&
+          (!isHigherPrecedence(oper_token, oper_stack.top())) &&
+          (!isOpeningParanthesis(oper_stack.top()))) {
+        postfix_expression += oper_stack.top();
+        postfix_expression += " ";
+        oper_stack.pop();
+      }
+      oper_stack.push(oper_token);
+    } else if (isClosingParanthesis(token)) {
+      // No need to push to stack. pop the top element as it should be
+      // an opening paranthesis.
+      while ((!oper_stack.empty()) &&
+          (!isOpeningParanthesis(oper_stack.top()))) {
+        postfix_expression += oper_stack.top();
+        postfix_expression += " ";
+        oper_stack.pop();
+      }
+      try {
+        oper_stack.pop();
+      } catch (exception e) {
+        cout << "Exception: " << e.what() << endl;
+      }
+    } else if (isOpeningParanthesis(token)) {
+      oper_stack.push('(');
+    } else {
+      // operands stay in order relative to each other.
+      postfix_expression += token;
+      postfix_expression += " ";
+    }
+  }
+  
+  // pop the remaining operators in the stack
+  while (!oper_stack.empty()) {
+    postfix_expression += oper_stack.top();
+    postfix_expression += " ";
+    oper_stack.pop();
+  }
+  return postfix_expression;
+}
--- test/expression_evaluation_unittest.cpp
+++ test/expression_evaluation_unittest.cpp
@@ -9,3 +9,53 @@ TEST (ExpressionEvaluationTest, postfixEvaluation) {
   float result = evaluatePostfix<float>(expression);
   EXPECT_EQ(result, 0.5);
 }
+
+TEST (ExpressionEvaluationTest, infixToPostfix) {
+  string expression = "2.0 * 3.0 + 4 * 1.0 - 9.5";
+  cout << "Infix expression: " << endl;
+  cout << expression << endl;
+
+  string postfix_expression = infixToPostfix(expression);
+  cout << "Postfix expression: " << endl;
+  cout << postfix_expression << endl;
+  EXPECT_EQ(postfix_expression, "2.0 3.0 * 4 1.0 * + 9.5 - ");
+  float result = evaluatePostfix<float>(postfix_expression);
+  EXPECT_EQ(result, 0.5);
+}
+
+TEST (ExpressionEvaluationTest, infixToPostfixEqualPrecedence) {
+  string expression = "2.0 * 4.0 / 4";
+  cout << "Infix expression: " << endl;
+  cout << expression << endl;
+
+  string postfix_expression = infixToPostfix(expression);
+  cout << "Postfix expression: " << endl;
+  cout << postfix_expression << endl;
+  EXPECT_EQ(postfix_expression, "2.0 4.0 * 4 / ");
+}
+
+TEST (ExpressionEvaluationTest, infixToPostfixParanthesis1) {
+  string expression = "2.0 * ( 4.0 + 3.5 )";
+  cout << "Infix expression: " << endl;
+  cout << expression << endl;
+
+  string postfix_expression = infixToPostfix(expression);
+  cout << "Postfix expression: " << endl;
+  cout << postfix_expression << endl;
+  EXPECT_EQ(postfix_expression, "2.0 4.0 3.5 + * ");
+  float result = evaluatePostfix<float>(postfix_expression);
+  EXPECT_EQ(result, 15);
+}
+
+TEST (ExpressionEvaluationTest, infixToPostfixParanthesis2) {
+  string expression = "( ( 4.0 + 3.5 ) * 2.0 - 12 ) * 2";
+  cout << "Infix expression: " << endl;
+  cout << expression << endl;
+
+  string postfix_expression = infixToPostfix(expression);
+  cout << "Postfix expression: " << endl;
+  cout << postfix_expression << endl;
+  EXPECT_EQ(postfix_expression, "4.0 3.5 + 2.0 * 12 - 2 * ");
+  float result = evaluatePostfix<float>(postfix_expression);
+  EXPECT_EQ(result, 6);
+}
